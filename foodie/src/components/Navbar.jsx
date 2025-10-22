0636ad61c91d38a7657ffca642c25bebaa28b973
deepa <deepacleetus182@gmail.com>
2025-10-22 19:18:06 +0530

    first commit

---
+++ foodie/src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { MdOutlineEmojiFoodBeverage } from 'react-icons/md';
+import { PiBowlFoodBold } from 'react-icons/pi';
+import { BiSearchAlt2 } from 'react-icons/bi';
+import { HiOutlineShoppingCart } from 'react-icons/hi';
+
+function Navbar() {
+  return (
+    <div className='w-full h-[100px] flex justify-between items-center px-8 bg-slate-200'>
+        <div className='w-[60px] h-[60px] bg-white flex justify-center items-center'>
+            <PiBowlFoodBold/>
+        </div>
+        <form action className="flex items-center bg-white rounded-full px-4 py-2 w-[400px] shadow-sm"
+>
+        <BiSearchAlt2/>
+        <input type="text" placeholder='search items..' className="ml-3 grow outline-none"/>
+        
+        </form>
+         <button type="button" className="ml-3 text-gray-600 relative">
+        <HiOutlineShoppingCart />
+         <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full px-1">0</span>
+        </button>
+
+
+
+
+    </div>
+  )
+}
+
+export default Navbar 
